90b49a65aa1d0d43014648e587ee3cf8e355f235
Teodorasss <ioanasanda@yahoo.com>
2025-09-21 02:38:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Găsește butonul din pagină
+const btn = document.getElementById("btn");
+
+// Adaugă o acțiune când e apăsat
+btn.addEventListener("click", () => {
+  alert("Felicitări! Ai făcut primul tău site!");
+});
